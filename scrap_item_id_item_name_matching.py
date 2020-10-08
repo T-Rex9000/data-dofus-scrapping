c436bf7d18ff85171fca0125164bd35f122089d2
Julien <julien.couyoupetrou@orange.fr>
2020-10-08 09:42:28 +0200

    Scrapping from the website of all the id/item_name pairs

--- scrap_item_id_item_name_matching.py
+++ scrap_item_id_item_name_matching.py
@@ -4,37 +4,27 @@ import json
 import time
 
 if __name__ == "__main__":
-    # Get DataFrame with all charcter csv files merged
-    df = fetch_all_breeds()
+    output_dct = {}
+    list_of_items = []
+    list_of_url_recipes = []
+    item_types = ["equipements", "armes"]
+    list_nb_pages = [25, 8]
 
-    # Drop entirely empty equipments
-    df.dropna(inplace=True, how="all")
+    for item_type, nb_pages in zip(item_types, list_nb_pages):
+        for page in range(1, nb_pages + 1):
+            url = "https://www.dofus.com/fr/mmorpg/encyclopedie/{}?size=96&page={}".format(item_type, page)
+            try:
+                page = requests.get(url)
+                soup = BeautifulSoup(page.content, 'html.parser')
+                table_items = list(soup.find_all('tbody'))
+                for i, link in enumerate(table_items[0].find_all('a')):
+                    if i % 2 != 0:
+                        id_item = link.get('href').split('/')[-1].split("-")[0]
+                        nom_item = link.text
+                        if id_item not in output_dct.keys():
+                            output_dct[id_item] = nom_item
+            except Exception as e:
+                print(e)
 
-    # Replace empty equipments with -1 (later mapped to unknown)
-    df.fillna(-1, inplace=True)
-
-    # Gather all item ids
-    id_list = []
-
-    for c in ['shield', 'amulet', 'ring1', 'cap', 'boots', 'weapon', 'hat', 'ring2', 'belt']:
-        id_list += list(set(df[c]))
-
-    # Start scrapping item names
-    out_dct = {}
-
-    for id in tqdm(id_list):
-        time.sleep(1)
-        url = f"https://www.dofus.com/fr/mmorpg/encyclopedie/equipements/{int(id)}"
-        try:
-            soup = url_to_soup(url)
-            title = soup.find("h1").text.replace("\n", "")
-
-            out_dct[int(id)] = title
-            print(title)
-
-        except Exception as e:
-            print(e)
-
-    # Save
     with open("scrapping/item_id_item_name.json", "w") as f:
-        json.dump(out_dct, f)
+        json.dump(output_dct, f)
--- utils.py
+++ utils.py
@@ -1,9 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
-
+import pandas as pd
 
 def url_to_soup(url):
     r = requests.get(url)
     data = r.text
     soup = BeautifulSoup(data)
     return soup
+
+
+def fetch_all_breeds():
+    breed_dct = {"6": "Ecaflip",
+                 "7": "Eniripsa",
+                 "8": "Iop",
+                 "9": "Cra",
+                 "1": "Feca",
+                 "11": "Sacrieur",
+                 "10": "Sadida",
+                 "2": "Osamodas",
+                 "3": "Enutrof",
+                 "4": "Sram",
+                 "5": "Xelor",
+                 "12": "Pandawa",
+                 "13": "Roublard",
+                 "14": "Zobal",
+                 "15": "Steamer",
+                 "16": "Eliotrope",
+                 "17": "Huppermage",
+                 "18": "Ouginak"}
+
+    # Merge all character csv files
+    df = []
+
+    for breed_id, breed in breed_dct.items():
+        try:
+            temp_df = pd.read_csv(f"character_lists/parsed_{breed}.csv")
+            df.append(temp_df)
+        except Exception as e:
+            print(e)
+
+    df = pd.concat(df)
+    return df
